f3b2b83533750a85b00fcd25de6e0fb2b6b1673a
CORP\linglzhu <zlinglin@volvocars.com>
2021-11-11 13:56:34 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-config-provider prefixCls="cns">
+    <section id="cns-main-app">
+      <section class="cns-menu-wrapper">
+        <main-menu :menus="menus" />
+      </section>
+      <section class="cns-frame-wrapper">
+        <!-- 主应用渲染区，用于挂载主应用路由触发的组件 -->
+        <router-view v-show="$route.name" />
+
+        <!-- 子应用渲染区，用于挂载子应用节点 -->
+        <section v-show="!$route.name" id="frame"></section>
+      </section>
+    </section>
+  </a-config-provider>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import MainMenu from "@/components/menu/index.vue";
+@Component({
   components: {
-    HelloWorld
-  }
+    MainMenu,
+  },
+})
+export default class App extends Vue {
+  /**
+   * 菜单列表
+   * key: 唯一 Key 值
+   * title: 菜单标题
+   * path: 菜单对应的路径
+   */
+  menus = [
+    {
+      key: "Home",
+      title: "主页",
+      path: "/",
+    },
+  ];
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+#cns-main-app {
+  height: 100%;
+  position: relative;
+  .cns-menu-wrapper {
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 40;
+    width: 172px;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+  .cns-nav-wrapper {
+    position: fixed;
+    width: 100%;
+    min-width: 1060px;
+    padding-left: 172px;
+    left: 0;
+    top: 0;
+    z-index: 30;
+  }
+}
+.cns-frame-wrapper {
+  padding-left: 172px;
+  flex-grow: 1;
+  height: 100%;
+  width: 100%;
+  position: relative;
+}
+#cns-frame {
+  width: 100%;
+  height: 100%;
+  > :first-child {
+    height: 100%;
+  }
 }
 </style>
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import Home from "@/pages/home/index.vue";
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+]
+
+export default routes;
